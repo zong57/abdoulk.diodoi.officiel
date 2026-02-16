6a3c4e9edcb6c4a55ac27215716fcfee2fd02110
Abdoulk Diodoi <nes.zon36@gmail.com>
2026-02-16 07:24:47 +0000

    Update service-worker.js
    Signed-off-by: Abdoulk Diodoi <nes.zon36@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,17 @@
-const CACHE = 'abdoulk-v1';
+const CACHE_NAME = 'app-a-platinum-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://www.chosic.com',
+  'https://assets.mixkit.co',
+  'https://assets.mixkit.co'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['/','/index.html','/manifest.json'])));
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
